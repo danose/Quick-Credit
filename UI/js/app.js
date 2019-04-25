f4b1570ac95e7bfa530b3ad68e3861b17c6cfdc2
unknown <etiobhiodaniel@gmail.com>
2019-04-25 12:11:32 +0100

    feature(dark mode):modify css [Finishes #165506997]

--- UI/js/app.js
+++ UI/js/app.js
@@ -11,3 +11,42 @@ exit.addEventListener('click', (e)=>{
     nav.style.display='none';
     e.preventDefault();
 });
+
+
+let trans = () => {
+    document.documentElement.classList.add('transition');
+    window.setTimeout(() => {
+        document.documentElement.classList.remove('transition')
+    }, 1000)
+}
+
+
+
+const darkMode=()=>{
+    document.documentElement.setAttribute('data-theme', 'dark');
+    trans();
+    window.localStorage.setItem("mode", "dark")
+}
+
+const lightMode =()=>{
+    document.documentElement.setAttribute('data-theme', 'light');
+   trans();
+    window.localStorage.setItem("mode", "light")
+}
+
+if(window.localStorage.getItem("mode")=="dark")
+    darkMode();
+else
+    lightMode();
+
+
+let checkbox = document.querySelector('input[name=theme]')   
+checkbox.addEventListener('click', function(){
+
+    if(this.checked){
+        darkMode();
+    } else{
+        lightMode();
+    }
+
+});
