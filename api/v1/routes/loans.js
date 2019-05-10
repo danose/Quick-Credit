802b8d95650f2ff86ba53fe98fd67147ea798b14
unknown <etiobhiodaniel@gmail.com>
2019-05-10 07:07:50 +0100

    feature(root handler and code comments)

--- api/v1/routes/loans.js
+++ api/v1/routes/loans.js
@@ -6,10 +6,10 @@ import admin from '../middleware/admin';
 
 const router = express.Router();
 
-router.post('/loans', auth, validateLoan, loanController.createLoan);
-router.get('/loans/:loanId', [auth, admin], loanController.getLoan);
-router.patch('/loans/:loanId', [auth, admin], loanController.approveRejectLoans);
-router.get('/loans', [auth, admin], loanController.getLoans);
+router.post('/loans', auth, validateLoan, loanController.createLoan); // post loan application
+router.get('/loans/:loanId', [auth, admin], loanController.getLoan); // Get a single loan application
+router.patch('/loans/:loanId', [auth, admin], loanController.approveRejectLoans); // Approve or reject loan applications
+router.get('/loans', [auth, admin], loanController.getLoans); // Get all loans, unpaid and repaid loans
 
 
 export default router;
--- api/v1/routes/repayment.js
+++ api/v1/routes/repayment.js
@@ -6,8 +6,8 @@ import admin from '../middleware/admin';
 
 const router = express.Router();
 
-router.post('/loans/:loanId/repayment', [auth, admin], validateRepayment, repaymentController.createRepayment);
-router.get('/loans/:loanId/repayments', auth, repaymentController.getRepayment);
+router.post('/loans/:loanId/repayment', [auth, admin], validateRepayment, repaymentController.createRepayment); // Post loan repayments
+router.get('/loans/:loanId/repayments', auth, repaymentController.getRepayment); // Get loan repayment history
 
 
 export default router;
