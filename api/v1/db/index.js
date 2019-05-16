4fd65ec27242e3a8e6e1f6f16847eaf731e720d4
unknown <etiobhiodaniel@gmail.com>
2019-05-16 19:38:34 +0100

    chore(setting up node postgress):add db [Finishes #166065218]

---
+++ api/v1/db/index.js
@@ -0,0 +1,37 @@
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const pool = new Pool({
+
+  connectionString: process.env.DATABASE_URL
+    
+});
+
+pool.on('connect', () => {
+  console.log('connected to the db');
+});
+
+export default {
+  /**
+     * Database Query
+     * @param {string} text
+     * @param {Array} params
+     * @returns {object} object
+     */
+
+  query(text, params) {
+    return new Promise((resolve, reject) => {
+      pool.query(text, params)
+        .then((res) => {
+          resolve(res);
+        })
+
+        .catch((err) => {
+          reject(err);
+        });
+    });
+  }
+
+};
