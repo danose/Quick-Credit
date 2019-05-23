5d58726130ae8c3e2365bcf3c0aeb2bb7df6a6b5
unknown <etiobhiodaniel@gmail.com>
2019-05-23 03:06:11 +0100

    feature(get loans) [Finishes #166201229]

--- api/v1/middleware/loanValidation.js
+++ api/v1/middleware/loanValidation.js
@@ -1,70 +1,32 @@
 /* eslint-disable import/prefer-default-export */
 
-// Validating the loan application
+/**
+     * Validate loans
+     * @param {object} req
+     * @param {object} res
+     * @param {object} next
+     * @returns {object} response object
+     */
 export const validateLoan = (req, res, next) => {
-
   const { tenor, amount } = req.body;
-
-  let error;
-
-  if (!tenor) {
-
-    error = 'tenor is required';
-
-  }
-
-  if (tenor && parseInt(tenor, 10) < 1) {
-
-    error = 'tenor should be between 1 and 12';
-
-  }
-
-  if (tenor && !tenor.match(/^[0-9]+$/)) {
-
-    error = 'tenor must be a number';
-
-  }
-
-  if (tenor && parseInt(tenor, 10) > 12) {
-
-    error = 'tenor should be between 1 and 12';
-
-  }
-
-  if (!amount) {
-
-    error = 'loan amount is required';
-
-  }
-  if (amount && !amount.match(/^[0-9]+$/)) {
-
-    error = 'amount must be a number';
-
-  }
-
-  if (amount && parseFloat(amount) < 10000) {
-
-    error = 'loan amount should be between 10000 to 200000';
-
-  }
-
-  if (amount && parseFloat(amount) > 200000) {
-
-    error = 'loan amount should be between 10000 to 200000';
-    
-  }
-
-  if (!error) {
-
+  const error = {};
+  if (!tenor) { error.tenor = 'tenor is required'; }
+  if (tenor && parseInt(tenor, 10) < 1) { error.tenor = 'tenor should be between 1 and 12'; }
+  if (tenor && !tenor.match(/^[0-9]+$/)) { error.tenor = 'tenor must be a number'; }
+  if (tenor && parseInt(tenor, 10) > 12) { error.tenor = 'tenor should be between 1 and 12'; }
+  if (!amount) { error.amount = 'loan amount is required'; }
+  if (amount && !amount.match(/^[0-9]+$/)) { error.amount = 'amount must be a number'; }
+  if (amount && parseFloat(amount) < 10000) { error.amount = 'loan amount should be between 10000 to 200000'; }
+  if (amount && parseFloat(amount) > 200000) { error.amount = 'loan amount should be between 10000 to 200000'; }
+  if (amount > 9999999) { error.amount = 'amount way above limit'; }
+  if (Object.keys(error).length === 0 && error.constructor === Object){
     return next();
-
   }
+
 
   return res.status(400)
     .json({
       status: 400,
       error
     });
-
- 
 };
