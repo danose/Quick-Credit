85a7abc2388e2137b38b03af7efcbc092fd5d284
unknown <etiobhiodaniel@gmail.com>
2019-05-23 03:30:49 +0100

    feature(authorizing and authentication

--- api/v1/middleware/admin.js
+++ api/v1/middleware/admin.js
@@ -1,18 +1,19 @@
-
+/**
+     * Authorize an admin
+     * @param {object} req
+     * @param {object} res
+     * @param {object} next
+     * @returns {object} response object
+     */
 const admin = (req, res, next) => {
-  
-  // Authorizing the Admin
   if (!req.user.isAdmin) {
-
     return res.status(403)
       .json({
         status: 403,
         error: 'Access Denied'
       });
-    
   }
   return next();
-
 };
 
 export default admin;
--- api/v1/middleware/auth.js
+++ api/v1/middleware/auth.js
@@ -3,43 +3,39 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-// Authenticating Users
+/**
+     * Authenticate a user
+     * @param {object} req
+     * @param {object} res
+     * @param {object} next
+     * @returns {object} response object
+     */
 const secret = process.env.SECRET;
 
 const auth = (req, res, next) => {
-
   const token = req.header('x-auth-access');
   if (!token) {
-        
-    res.status(401)
-      .json({
-        status: 401,
-        error: 'Access denied. No token provided'
-      });
-  
-
+    res.status(401).json({ status: 401, error: 'Access denied. No token provided' });
   }
-
-  
   try {
-
     const decoded = jwt.verify(token, secret);
     req.user = {
       id: decoded.userId,
-      isAdmin: decoded.isAdmin
+      isAdmin: decoded.isAdmin,
+      email: decoded.email,
+      firstName: decoded.firstName,
+      lastName: decoded.lastName,
+      password: decoded.password,
+      address: decoded.address,
+      phone: decoded.phone
     };
     next();
-
   } catch (ex) {
-
     res.status(400)
       .json({
         status: 400,
         error: 'Invalid token'
       });
-
   }
-
 };
-
 export default auth;
