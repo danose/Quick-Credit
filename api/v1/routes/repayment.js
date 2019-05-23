c76216074428ae1e0a35149f5a6ef34884c6b75b
unknown <etiobhiodaniel@gmail.com>
2019-05-23 03:39:11 +0100

    feature(users can view repayments)[Finishes #166201903]

--- api/v1/routes/repayment.js
+++ api/v1/routes/repayment.js
@@ -4,10 +4,11 @@ import { validateRepayment } from '../middleware/repaymentValidation';
 import auth from '../middleware/auth';
 import admin from '../middleware/admin';
 import adminPrevent from '../middleware/preventAdmin';
+import { checkPaidAmount } from '../middleware/checkAmount';
 
 const router = express.Router();
 
-router.post('/loans/:loanId/repayment', [auth, admin], validateRepayment, repaymentController.createRepayment); // Post loan repayments
+router.post('/loans/:loanId/repayment', [auth, admin], checkPaidAmount, validateRepayment, repaymentController.createRepayment); // Post loan repayments
 router.get('/loans/:loanId/repayments', [auth, adminPrevent], repaymentController.getRepayment); // Get loan repayment history
 
 
